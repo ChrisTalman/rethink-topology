21646980034c4eea082ad26628353207841aecac
Chris Talman <cewe.wow@gmail.com>
2018-07-27 21:16:38 +0100

    Incorporated guaranteeIndexes().

--- src/Modules/Evaluate/GuaranteeTable.ts
+++ src/Modules/Evaluate/GuaranteeTable.ts
@@ -3,38 +3,46 @@
 // External Modules
 import RethinkDB from 'rethinkdb';
 
+// Internal Modules
+import guaranteeIndexes from './GuaranteeIndexes';
+
 // Types
 import { Topology } from 'src/Types';
 import { TableList } from './GuaranteeTables';
 
-export default async function(table: Topology.Table, tableList: TableList, topology: Topology.Base, connection)
+export default async function(table: Topology.Table, tableList: TableList, topology: Topology.Base, connection: RethinkDB.Connection)
 {
-	const tableExists = tableList.includes(table.name)
-	if (!tableExists)
+	const tableGuaranteed = await guarantee(table, tableList, topology, connection);
+	if (!tableGuaranteed)
 	{
-		log('Creating...', table);
-		const created = await create(table, topology, connection);
-		if (!created)
-		{
-			return;
-		};
-		log('Created.', table);
+		return;
+	};
+	const indexesGuaranteed = await guaranteeIndexes(table, connection);
+	if (!indexesGuaranteed)
+	{
+		return;
 	};
 	return true;
 };
 
-async function create(table: Topology.Table, topology: Topology.Base, connection)
+async function guarantee(table: Topology.Table, tableList: TableList, topology: Topology.Base, connection: RethinkDB.Connection)
 {
-	const query = RethinkDB
-		.tableCreate(table.name, {shards: topology.shards, replicas: topology.replicas});
-	try
+	const exists = tableList.includes(table.name);
+	if (!exists)
 	{
-		await query.run(connection);
-	}
-	catch (error)
-	{
-		logError('Failed creation: ' + error.message, table);
-		return;
+		log('Creating...', table);
+		const query = RethinkDB
+			.tableCreate(table.name, {shards: topology.shards, replicas: topology.replicas});
+		try
+		{
+			await query.run(connection);
+		}
+		catch (error)
+		{
+			logError('Failed creation: ' + error.message, table);
+			return;
+		};
+		log('Created.', table);
 	};
 	return true;
 };
@@ -44,7 +52,7 @@ function log(message: string, table: Topology.Table)
 	console.log('[Table] [' + table.name + '] ' + message);
 };
 
-function logError(message: string, table: Topology.Table)
+export function logError(message: string, table: Topology.Table)
 {
 	console.error('[Table] [' + table.name + '] ' + message);
 };
