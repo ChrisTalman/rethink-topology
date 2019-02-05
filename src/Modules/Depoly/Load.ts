53d27f290cc2791db919deb2d791e40b72b25f5f
Chris Talman <cewe.wow@gmail.com>
2019-02-05 17:48:01 +0000

    Passed error to TopologyEvaluationError, instead of error message.

--- src/Modules/Depoly/Load.ts
+++ src/Modules/Depoly/Load.ts
@@ -125,7 +125,7 @@ export default async function load()
 	}
 	catch (error)
 	{
-		throw new TopologyEvaluationError(error.message);
+		throw new TopologyEvaluationError(error);
 	};
 	const validated = Joi.validate(topology, SCHEMA);
 	if (validated.error) throw new TopologySchemaError(validated.error.message);
