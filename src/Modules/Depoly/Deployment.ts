fd074fbe519970b874f7b0ceb954cf687e966ee3
Chris Talman <cewe.wow@gmail.com>
2019-02-05 17:15:22 +0000

    Changed schema to allow string or object for RethinkDB options property.

--- src/Modules/Depoly/Deployment.ts
+++ src/Modules/Depoly/Deployment.ts
@@ -7,34 +7,34 @@ import * as Joi from 'joi';
 import Config from '@bluecewe/config';
 
 // Constants
+const RETHINK_OPTIONS_SCHEMA = Joi.object
+	(
+		{
+			host: Joi.string().optional(),
+			port: Joi.number().optional(),
+			server: Joi.object().optional(),
+			db: Joi.string().optional(),
+			user: Joi.string().optional(),
+			password: Joi.string().optional(),
+			discovery: Joi.boolean().optional(),
+			pool: Joi.boolean().optional(),
+			buffer: Joi.number().optional(),
+			max: Joi.number().optional(),
+			timeout: Joi.number().optional(),
+			pingInterval: Joi.number().optional(),
+			timeoutError: Joi.number().optional(),
+			timeoutGb: Joi.number().optional(),
+			maxExponent: Joi.number().optional(),
+			silent: Joi.boolean().optional(),
+			log: Joi.func().optional()
+		}
+	);
 const OPTIONS_SCHEMA = Joi.object
 	(
 		{
 			deleteDefaultDatabase: Joi.boolean().default(true),
 			log: Joi.boolean().default(false),
-			rethink: Joi.object
-				(
-					{
-						host: Joi.string().optional(),
-						port: Joi.number().optional(),
-						server: Joi.object().optional(),
-						db: Joi.string().optional(),
-						user: Joi.string().optional(),
-						password: Joi.string().optional(),
-						discovery: Joi.boolean().optional(),
-						pool: Joi.boolean().optional(),
-						buffer: Joi.number().optional(),
-						max: Joi.number().optional(),
-						timeout: Joi.number().optional(),
-						pingInterval: Joi.number().optional(),
-						timeoutError: Joi.number().optional(),
-						timeoutGb: Joi.number().optional(),
-						maxExponent: Joi.number().optional(),
-						silent: Joi.boolean().optional(),
-						log: Joi.func().optional()
-					}
-				)
-				.required()
+			rethink: Joi.alternatives(Joi.string(), RETHINK_OPTIONS_SCHEMA).required()
 		}
 	)
 	.required()
