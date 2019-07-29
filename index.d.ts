f27ff519b251349cb6ba7d7b53238bfefe76118b
Chris Talman <cewe.mail@gmail.com>
2019-07-29 21:26:18 +0100

    Fixed indentation.

--- index.d.ts
+++ index.d.ts
@@ -4,16 +4,16 @@
 import { RConnectionOptions } from 'rethinkdb-ts';
 declare module '@bluecewe/rethink-topology'
 {
-    export interface Options
-    {
-    	/** Determines whether the default database 'tests' should be deleted. */
-    	deleteDefaultDatabase?: boolean;
-    	/** Log debugging events to console.log(). */
-    	log?: boolean;
-    	/** RethinkDB connection options. If string, used as JSON file path from which to obtain connection object. */
-    	rethink: string | RConnectionOptions;
-    }
-    /** Loads topology from default location and deploys it to the database provided in the options. */
-    export function deploy(options: Options): Promise<void>;
-    export class ConnectionConfigFileError extends Error {}
+	export interface Options
+	{
+		/** Determines whether the default database 'tests' should be deleted. */
+		deleteDefaultDatabase?: boolean;
+		/** Log debugging events to console.log(). */
+		log?: boolean;
+		/** RethinkDB connection options. If string, used as JSON file path from which to obtain connection object. */
+		rethink: string | RConnectionOptions;
+	}
+	/** Loads topology from default location and deploys it to the database provided in the options. */
+	export function deploy(options: Options): Promise<void>;
+	export class ConnectionConfigFileError extends Error {}
 }
--- src/App/Types/Topology.ts
+++ src/App/Types/Topology.ts
@@ -5,79 +5,79 @@ import { RDatum } from 'rethinkdb-ts';
 
 export interface Topology
 {
-    shards?: number;
-    replicas?: number;
-    users: GlobalUsers;
-    databases: Databases;
+	shards?: number;
+	replicas?: number;
+	users: GlobalUsers;
+	databases: Databases;
 };
 export interface BaseUser
 {
-    username: string;
-    read?: boolean;
-    write?: boolean;
+	username: string;
+	read?: boolean;
+	write?: boolean;
 };
 export interface GlobalUsers extends Array<string | GlobalUser> {};
 export interface GlobalUser extends BaseUser
 {
-    connect?: boolean;
-    config?: boolean;
+	connect?: boolean;
+	config?: boolean;
 };
 export interface Databases extends Array<Database> {};
 export interface Database
 {
-    name: string;
-    shards?: number;
-    replicas?: number;
-    users: DatabaseUsers;
-    tables: Tables;
+	name: string;
+	shards?: number;
+	replicas?: number;
+	users: DatabaseUsers;
+	tables: Tables;
 };
 export interface DatabaseUsers extends Array<DatabaseUser> {};
 export interface DatabaseUser extends BaseUser
 {
-    config?: boolean;
+	config?: boolean;
 };
 export interface Tables extends Array<Table> {};
 export interface Table
 {
-    name: string;
-    shards?: number;
-    replicas?: number;
-    users: TableUsers;
-    indexes?: Indexes;
-    database: Database;
+	name: string;
+	shards?: number;
+	replicas?: number;
+	users: TableUsers;
+	indexes?: Indexes;
+	database: Database;
 };
 export interface TableUsers extends Array<TableUser> {};
 export interface TableUser extends BaseUser
 {
-    config?: boolean;
-    read?: boolean;
-    write?: boolean;
+	config?: boolean;
+	read?: boolean;
+	write?: boolean;
 };
 export interface Indexes extends Array<IndexVariant> {};
 export type IndexVariant =
-    string
-    | NameIndexVariant
-    | CompoundIndex
+	string
+	| NameIndexVariant
+	| CompoundIndex
 ;
 export type NameIndexVariant =
-    NameIndex
-    | NameConvertIndex
-    | NameArbitraryIndex
+	NameIndex
+	| NameConvertIndex
+	| NameArbitraryIndex
 ;
 export interface NameIndex
 {
-    name: string;
+	name: string;
 }
 export interface NameConvertIndex extends NameIndex
 {
-    convert: NumberConstructor;
+	convert: NumberConstructor;
 };
 export interface NameArbitraryIndex extends NameIndex
 {
-    arbitrary: (document: RDatum) => any;
+	arbitrary: (document: RDatum) => any;
 };
 export interface CompoundIndex
 {
-    compound: Array<CompoundIndexField>;
+	compound: Array<CompoundIndexField>;
 };
 export type CompoundIndexField = string | NameIndexVariant;
