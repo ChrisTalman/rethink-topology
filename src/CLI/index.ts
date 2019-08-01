a9b26b33b416504830f183d63daad3a3e9cfe119
Chris Talman <cewe.mail@gmail.com>
2019-08-01 13:00:11 +0100

    Introduced command option '--deleteUndeclaredIndexes'; introduced getCommandOptions().

--- src/CLI/index.ts
+++ src/CLI/index.ts
@@ -3,4 +3,24 @@
 // External Modules
 import deploy from 'src/App/Modules/Deploy';
 
-deploy({rethink: './config.json', log: true});
+deploy
+(
+	{
+		rethink: './config.json',
+		log: true,
+		deleteUndeclaredIndexes: getCommandOptions().includes('--deleteUndeclaredIndexes')
+	}
+);
+
+function getCommandOptions()
+{
+	const allOptions = process.argv.slice(2);
+	const nodeOptions = process.execArgv;
+	const commandOptions: Array<string> = [];
+	for (let option of allOptions)
+	{
+		if (nodeOptions.includes(option)) continue;
+		commandOptions.push(option);
+	};
+	return commandOptions;
+};
