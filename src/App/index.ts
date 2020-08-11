b3896be05db671c8cea541585acbed348b2aa06b
Chris Talman <cewe.mail@gmail.com>
2020-08-11 21:08:41 +0100

    Incorporated listenUnhandledErrors().

--- src/App/index.ts
+++ src/App/index.ts
@@ -1,5 +1,11 @@
 'use strict';
 
+// External Modules
+import { listenUnhandledErrors } from '@chris-talman/node-utilities';
+
 // Methods
 export { deploy } from 'src/App/Modules/Deploy';
 export { ConnectionConfigFileError } from 'src/App/Modules/Deploy/Deployment';
+
+// Listen Unhandled Errors
+listenUnhandledErrors();
